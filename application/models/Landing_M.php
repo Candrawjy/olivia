23d646f0fc1866a7d1f9e99dcb155eced4179d4b
Candrawjy <candraw71@gmail.com>
2022-10-24 16:02:43 +0700

    Can: update landing page

--- application/models/Landing_M.php
+++ application/models/Landing_M.php
@@ -18,12 +18,12 @@ class Landing_M extends CI_Model
 
     public function getDataJasa()
     {
-        $this->db->select('user.*, jenis_kategori.*, umkm_jasa.*');
-        $this->db->from('umkm_jasa');
-        $this->db->join('user', 'user.id_user = umkm_jasa.id_user');
-        $this->db->join('jenis_kategori', 'jenis_kategori.id_jeniskategori = umkm_jasa.id_jeniskategori');
+    	$this->db->select('umkm_jasa.*, jenis_kategori.*, produk.*');
+        $this->db->from('produk');
+        $this->db->join('umkm_jasa', 'umkm_jasa.id_umkm_jasa = produk.id_umkm_jasa');
+        $this->db->join('jenis_kategori', 'jenis_kategori.id_jeniskategori = produk.id_jeniskategori');
         $this->db->where('umkm_jasa.jenis', '2');
-        $this->db->order_by('umkm_jasa.created_at','DESC');
+        $this->db->order_by('produk.created_at','DESC');
         $this->db->limit(10, 'ASC');
         $query = $this->db->get();
         return $query;
